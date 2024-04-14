aadc8a5baf647fa6caa47bbce31ddf2365cba54c
abdul-majeed-khan <abdulmajeed301100@gmail.com>
2024-04-14 17:00:49 +0530

    tsx

--- src/slices/Hero/Shapes.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import * as THREE from "three"
-import { Canvas } from "@react-three/fiber"
-import { ContactShadows, Float, Environment } from "@react-three/drei"
-import { Suspense, useEffect, useRef, useState } from "react"
-import { gsap } from "gsap"
